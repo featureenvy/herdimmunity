4097ade48a6a75a72eec6ac29c3783a4fca625a8
Andreas Arnold <andreas@featureenvy.com>
2014-04-25 11:25:48 +0200

    Use a vector instead of a seq, since Om handles vectors.
    It doesn't seem to handle seqs, at least we won't get back cursors once accessing an element in the seq. It works with vectors, though.

--- src/herdimmunity/game.cljs
+++ src/herdimmunity/game.cljs
@@ -2,8 +2,8 @@
 
 (defn gen-board
   [size]
-  (let [values [:alive :infected :dead :empty]
-        gen-rows (fn [] (map #(rand-nth values) (range 1 size)))]
-    (map #(gen-rows) (range 1 size))))
+  (let [values [[:alive] [:infected] [:dead] [:empty]]
+        gen-rows (fn [] (vec (map #(rand-nth values) (range 1 size))))]
+    (vec (map #(gen-rows) (range 1 size)))))
 
 (def board (gen-board 20))
