af498be52ea0935d1cd6db64b48163a946f99378
Andreas Arnold <andreas@featureenvy.com>
2014-05-21 22:08:10 +0200

    Real3D!

--- src/herdimmunity/webgl.cljs
+++ src/herdimmunity/webgl.cljs
@@ -1,14 +1,15 @@
 (ns herdimmunity.webgl)
 
-(def triangle-vertex-position-buffer (atom []))
-(def triangle-vertex-color-buffer (atom []))
-(def square-vertex-position-buffer (atom []))
-(def square-vertex-color-buffer (atom []))
+(def pyramid-vertex-position-buffer (atom []))
+(def pyramid-vertex-color-buffer (atom []))
+(def cube-vertex-position-buffer (atom []))
+(def cube-vertex-color-buffer (atom []))
+(def cube-vertex-index-buffer (atom []))
 (def p-matrix (atom (.create js/mat4)))
 (def mv-matrix (atom (.create js/mat4)))
 (def mv-matrix-stack (atom []))
-(def r-tri (atom 0))
-(def r-square (atom 0))
+(def r-pyramid (atom 0))
+(def r-cube (atom 0))
 (def last-time (atom 0))
 (def gl-obj (atom 0))
 (def shader-program-obj (atom 0))
@@ -59,44 +60,102 @@
   (.uniformMatrix4fv gl (.-mvMatrixUniform shader-program) false @mv-matrix))
 
 (defn init-buffers [gl]
-  (let [t-vertices #js [0.0 1.0 0.0
-                  -1.0 -1.0 0.0
-                        1.0 -1.0 0.0]
-        t-colors #js [1.0 0.0 0.0 1.0
+  (let [p-vertices #js [; front face
+                        0 1 0
+                        -1 -1 1
+                        1 -1 1
+                                        ; right face
+                        0 1 0
+                        1 -1 1
+                        1 -1 -1
+                                        ; back face
+                        0 1 0
+                        1 -1 -1
+                        -1 -1 -1
+                                        ; left face
+                        0 1 0
+                        -1 -1 -1
+                        -1 -1 1]
+        p-colors #js [1.0 0.0 0.0 1.0 ; front face
                       0.0 1.0 0.0 1.0
-                      0.0 0.0 1.0 1.0]
-        s-vertices #js [1.0 1.0 0.0
-                        -1.0 1.0 0.0
-                        1.0 -1.0 0.0
-                        -1.0 -1.0 0.0]
-        s-colors #js [0.5 0.5 1.0 1.0
-                      0.5 0.5 1.0 1.0
-                      0.5 0.5 1.0 1.0
-                      0.5 0.5 1.0 1.0]]
-    (reset! triangle-vertex-position-buffer (.createBuffer gl))
-    (.bindBuffer gl (.-ARRAY_BUFFER gl) @triangle-vertex-position-buffer)
-    (.bufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array. t-vertices) (.-STATIC_DRAW gl))
-    (set! (.-itemSize @triangle-vertex-position-buffer) 3)
-    (set! (.-numItems @triangle-vertex-position-buffer) 3)
+                      0.0 0.0 1.0 1.0
+                      1 0 0 1 ; right face
+                      0 0 1 1
+                      0 1 0 1
+                      1 0 0 1 ; back face
+                      0 1 0 1
+                      0 0 1 1
+                      1 0 0 1 ; left face
+                      0 0 1 1
+                      0 1 0 1]
+        c-vertices #js [-1 -1 1 ; front face
+                        1 -1 1
+                        1 1 1
+                        -1 1 1
+                        -1 -1 -1 ; back face
+                        -1 1 -1
+                        1 1 -1
+                        1 -1 -1
+                        -1 1 -1 ; top face
+                        -1 1 1
+                        1 1 1
+                        1 1 -1
+                        -1 -1 -1 ; bottom face
+                        1 -1 -1
+                        1 -1 1
+                        -1 -1 1
+                        1 -1 -1 ; right face
+                        1 1 -1
+                        1 1 1
+                        1 -1 1
+                        -1 -1 -1 ; left face
+                        -1 -1 1
+                        -1 1 1
+                        -1 1 -1]
+        c-single-colors  [[1 0 0 1] ; front face
+                          [1 1 0 1] ; back face
+                          [0 1 0 1] ; top face
+                          [1 .5 .5 1] ; bottom face
+                          [1 0 1 1] ; right face
+                          [0 0 1 1] ; left face
+                          ]
+        c-colors (clj->js (flatten (map (fn [val] (repeatedly 4 #(identity val))) c-single-colors)))
+        c-indexes #js [0 1 2 0 2 3 ; front face
+                   4 5 6 4 6 7
+                   8 9 10 8 10 11
+                   12 13 14 12 14 15
+                   16 17 18 16 18 19
+                   20 21 22 20 22 23
+                   ]]
+    (reset! pyramid-vertex-position-buffer (.createBuffer gl))
+    (.bindBuffer gl (.-ARRAY_BUFFER gl) @pyramid-vertex-position-buffer)
+    (.bufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array. p-vertices) (.-STATIC_DRAW gl))
+    (set! (.-itemSize @pyramid-vertex-position-buffer) 3)
+    (set! (.-numItems @pyramid-vertex-position-buffer) 12)
 
-    (reset! triangle-vertex-color-buffer (.createBuffer gl))
-    (.bindBuffer gl (.-ARRAY_BUFFER gl) @triangle-vertex-color-buffer)
-    (.bufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array. t-colors) (.-STATIC_DRAW gl))
-    (set! (.-itemSize @triangle-vertex-color-buffer) 4)
-    (set! (.-numItems @triangle-vertex-color-buffer) 3)
+    (reset! pyramid-vertex-color-buffer (.createBuffer gl))
+    (.bindBuffer gl (.-ARRAY_BUFFER gl) @pyramid-vertex-color-buffer)
+    (.bufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array. p-colors) (.-STATIC_DRAW gl))
+    (set! (.-itemSize @pyramid-vertex-color-buffer) 4)
+    (set! (.-numItems @pyramid-vertex-color-buffer) 12)
 
-    (reset! square-vertex-position-buffer (.createBuffer gl))
-    (.bindBuffer gl (.-ARRAY_BUFFER gl) @square-vertex-position-buffer)
-    (.bufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array. s-vertices) (.-STATIC_DRAW gl))
-    (set! (.-itemSize @square-vertex-position-buffer) 3)
-    (set! (.-numItems @square-vertex-position-buffer) 4)
+    (reset! cube-vertex-position-buffer (.createBuffer gl))
+    (.bindBuffer gl (.-ARRAY_BUFFER gl) @cube-vertex-position-buffer)
+    (.bufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array. c-vertices) (.-STATIC_DRAW gl))
+    (set! (.-itemSize @cube-vertex-position-buffer) 3)
+    (set! (.-numItems @cube-vertex-position-buffer) 24)
 
-    (reset! square-vertex-color-buffer (.createBuffer gl))
-    (.bindBuffer gl (.-ARRAY_BUFFER gl) @square-vertex-color-buffer)
-    (.bufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array. s-colors) (.-STATIC_DRAW gl))
-    (set! (.-itemSize @square-vertex-color-buffer) 4)
-    (set! (.-numItems @square-vertex-color-buffer) 4)
-    ))
+    (reset! cube-vertex-color-buffer (.createBuffer gl))
+    (.bindBuffer gl (.-ARRAY_BUFFER gl) @cube-vertex-color-buffer)
+    (.bufferData gl (.-ARRAY_BUFFER gl) (js/Float32Array. c-colors) (.-STATIC_DRAW gl))
+    (set! (.-itemSize @cube-vertex-color-buffer) 4)
+    (set! (.-numItems @cube-vertex-color-buffer) 24)
+
+    (reset! cube-vertex-index-buffer (.createBuffer gl))
+    (.bindBuffer gl (.-ELEMENT_ARRAY_BUFFER gl) @cube-vertex-index-buffer)
+    (.bufferData gl (.-ELEMENT_ARRAY_BUFFER gl) (js/Uint16Array. c-indexes) (.-STATIC_DRAW gl))
+    (set! (.-itemSize @cube-vertex-index-buffer) 1)
+    (set! (.-numItems @cube-vertex-index-buffer) 36)))
 
 (defn draw-scene [gl shader-program]
   (.viewport gl 0 0 (.-viewportWidth gl) (.-viewportHeight gl))
@@ -107,29 +166,34 @@
   
   (.translate js/mat4 @mv-matrix @mv-matrix #js [-1.5 0.0 -7.0])
   (mv-push-matrix)
-  (.rotate js/mat4 @mv-matrix @mv-matrix (deg-to-rad @r-tri) #js [0 1 0])
-  (.bindBuffer gl (.-ARRAY_BUFFER gl) @triangle-vertex-position-buffer)
-  (.vertexAttribPointer gl (.-vertexPositionAttribute shader-program) (.-itemSize @triangle-vertex-position-buffer) (.-FLOAT gl) false 0 0)
+  (.rotate js/mat4 @mv-matrix @mv-matrix (deg-to-rad @r-pyramid) #js [0 1 0])
+  (.bindBuffer gl (.-ARRAY_BUFFER gl) @pyramid-vertex-position-buffer)
+  (.vertexAttribPointer gl (.-vertexPositionAttribute shader-program) (.-itemSize @pyramid-vertex-position-buffer) (.-FLOAT gl) false 0 0)
 
-  (.bindBuffer gl (.-ARRAY_BUFFER gl) @triangle-vertex-color-buffer)
-  (.vertexAttribPointer gl (.-vertexColorAttribute shader-program) (.-itemSize @triangle-vertex-color-buffer) (.-FLOAT gl) false 0 0)
+  (.bindBuffer gl (.-ARRAY_BUFFER gl) @pyramid-vertex-color-buffer)
+  (.vertexAttribPointer gl (.-vertexColorAttribute shader-program) (.-itemSize @pyramid-vertex-color-buffer) (.-FLOAT gl) false 0 0)
   
   (set-matrix-uniforms gl shader-program)
-  (.drawArrays gl (.-TRIANGLES gl) 0 (.-numItems @triangle-vertex-position-buffer))
+  (.drawArrays gl (.-TRIANGLES gl) 0 (.-numItems @pyramid-vertex-position-buffer))
 
   (mv-pop-matrix)
   
   (.translate js/mat4 @mv-matrix @mv-matrix #js [3.0 0.0 0.0])
   (mv-push-matrix)
-  (.rotate js/mat4 @mv-matrix @mv-matrix (deg-to-rad @r-square) #js [1 0 0])
-  (.bindBuffer gl (.-ARRAY_BUFFER gl) @square-vertex-position-buffer)
-  (.vertexAttribPointer gl (.-vertexPositionAttribute shader-program) (.-itemSize @square-vertex-position-buffer) (.-FLOAT gl) false 0 0)
+  (.rotate js/mat4 @mv-matrix @mv-matrix (deg-to-rad @r-cube) #js [1 1 1])
+  (.bindBuffer gl (.-ARRAY_BUFFER gl) @cube-vertex-position-buffer)
+  (.vertexAttribPointer gl (.-vertexPositionAttribute shader-program) (.-itemSize @cube-vertex-position-buffer) (.-FLOAT gl) false 0 0)
 
-  (.bindBuffer gl (.-ARRAY_BUFFER gl) @square-vertex-color-buffer)
-  (.vertexAttribPointer gl (.-vertexColorAttribute shader-program) (.-itemSize @square-vertex-color-buffer) (.-FLOAT gl) false 0 0)
-  
+  (.bindBuffer gl (.-ARRAY_BUFFER gl) @cube-vertex-color-buffer)
+  (.vertexAttribPointer gl (.-vertexColorAttribute shader-program) (.-itemSize @cube-vertex-color-buffer) (.-FLOAT gl) false 0 0)
+
+  (.bindBuffer gl (.-ELEMENT_ARRAY_BUFFER gl) @cube-vertex-index-buffer)
   (set-matrix-uniforms gl shader-program)
-  (.drawArrays gl (.-TRIANGLE_STRIP gl) 0 (.-numItems @square-vertex-position-buffer)  (mv-pop-matrix)))
+  (.drawElements gl (.-TRIANGLES gl) (.-numItems @cube-vertex-index-buffer) (.-UNSIGNED_SHORT gl) 0)
+  ;(.drawArrays gl (.-TRIANGLE_STRIP gl) 0 (.-numItems @cube-vertex-position-buffer))
+
+  (mv-pop-matrix)
+  )
 
 (defn animate
   []
@@ -137,8 +201,8 @@
         elapsed (- time-now @last-time)]
     (if (not= @last-time 0)
       (do
-        (reset! r-tri (+ @r-tri (/ (* 90 elapsed) 1000)))
-        (reset! r-square (+ @r-square (/ (* 75 elapsed) 1000)))))
+        (reset! r-pyramid (+ @r-pyramid (/ (* 90 elapsed) 1000)))
+        (reset! r-cube (- @r-cube (/ (* 75 elapsed) 1000)))))
     (reset! last-time time-now)))
 
 (defn tick
