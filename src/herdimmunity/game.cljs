663cc1419bc49d3a4599b461825107aa012c40b2
Andreas Arnold <andreas@featureenvy.com>
2014-06-11 21:52:21 +0200

    Remove the vec calls, they arent actually needed.

--- src/herdimmunity/game.cljs
+++ src/herdimmunity/game.cljs
@@ -3,8 +3,8 @@
 (defn gen-board
   [size]
   (let [values [[:alive] [:infected] [:dead] [:empty]]
-        gen-rows (fn [] (vec (map #(rand-nth values) (range 1 size))))]
-    (vec (map #(gen-rows) (range 1 size)))))
+        gen-rows (fn [] (map #(rand-nth values) (range 1 size)))]
+     (map #(gen-rows) (range 1 size))))
 
 (defn neighbors [x y]
   (for [dx [-1 0 1] dy [-1 0 1]
@@ -21,5 +21,5 @@
 
 (defn step
   [board]
-  (vec (map-indexed (fn [row-idx row] (vec (map-indexed #(transition-cell %1 row-idx board) row)))
-                    board)))
+  (map-indexed (fn [row-idx row] (map-indexed #(transition-cell %1 row-idx board) row))
+                 board))
